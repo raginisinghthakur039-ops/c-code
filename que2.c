33a2903f860c342f7bc58568c6ada3ab255e3643
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-10-13 21:38:39 +0530

    array
    array

---
+++ que2.c
@@ -0,0 +1,12 @@
+    //reverse an array in place
+#include <stdio.h>
+int main(){
+    int arr[5]={1,2,3,6,7};
+    //printf("%d",arr[45]);
+    for(int i=4;i>=0;i--){
+     printf("%d",arr[i]);
+    }
+    return 0;
+}
+
+//output:7,6,3,2,1
