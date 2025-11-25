b56177d0879e253227d94411e07779edee1c527a
raginisinghthakur039-ops <raginisinghthakur039@gmail.com>
2025-11-25 21:50:30 +0530

    check palindrom or not
    check palindrom or not

---
+++ que22.c
@@ -0,0 +1,23 @@
+//check for palindron
+#include <stdio.h>
+ #include <string.h>
+ int main(){
+    char name[]="madam";
+    int ln= strlen(name);
+    int isTrue=1;
+    for(int i=0;i<ln/2;i++){
+        if(name[i]!=name[ln-i-1]){
+            isTrue=0;
+            break;
+        }
+    }
+    if (isTrue==1)
+    {
+        printf("palindrom ");
+    }
+    else{
+        printf("not palinddrom");
+    }
+    return 0;
+
+ }
